dcb0b1326f6f0bc7527ceada5f3bbff6bccad679
Henrik Hallberg <henrik.hallberg@sweco.se>
2019-11-05 16:10:15 +0100

    Added functionality to create geometries from markis

--- new-client/src/plugins/markis/MarkisView.js
+++ new-client/src/plugins/markis/MarkisView.js
@@ -4,8 +4,13 @@ import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
 import { withSnackbar } from "notistack";
 import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
 
 const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
   root: {
     display: "flex",
     flexWrap: "wrap"
@@ -19,6 +24,14 @@ const styles = theme => ({
   },
   row: {
     marginBottom: theme.spacing(1)
+  },
+  createButtons: {
+    margin: theme.spacing(1)
+  },
+  textField: {
+    marginLeft: 0,
+    marginRight: 0,
+    width: "100%"
   }
 });
 
@@ -29,7 +42,9 @@ class MarkisView extends React.PureComponent {
     mode: "visningsläge",
     contractId: "",
     contractType: "",
-    enableCreate: false
+    enableCreate: false,
+    inCreation: false,
+    formValues: {}
   };
 
   constructor(props) {
@@ -42,6 +57,7 @@ class MarkisView extends React.PureComponent {
 
     this.localObserver.subscribe("markisErrorEvent", message => {
       this.showAdvancedSnackbar(message);
+      this.reset();
     });
     this.localObserver.subscribe("markisSearchComplete", message => {
       this.props.enqueueSnackbar(message);
@@ -65,6 +81,88 @@ class MarkisView extends React.PureComponent {
         enableCreate: message.enabled
       });
     });
+    this.localObserver.subscribe("editFeature", attr => {
+      this.setState({
+        editFeature: this.props.model.editFeature,
+        editSource: this.props.model.editSource
+      });
+    });
+  }
+
+  checkText(name, value) {
+    var formValues = Object.assign({}, this.state.formValues);
+    formValues[name] = value;
+    this.setState({
+      formValues: formValues
+    });
+  }
+
+  updateFeature() {
+    var props = this.props.model.editFeature.getProperties();
+    Object.keys(this.state.formValues).forEach(key => {
+      var value = this.state.formValues[key];
+      if (value === "") value = null;
+      if (Array.isArray(value)) {
+        value = value
+          .filter(v => v.checked)
+          .map(v => v.name)
+          .join(";");
+      }
+      props[key] = value;
+    });
+    this.props.model.editFeature.setProperties(props);
+  }
+
+  getValueMarkup(field) {
+    const { classes } = this.props;
+    if (field.dataType === "int") {
+      field.textType = "heltal";
+    }
+
+    if (field.dataType === "number") {
+      field.textType = "nummer";
+    }
+
+    if (field.dataType === "date") {
+      field.textType = "datum";
+    }
+
+    var value = this.state.formValues[field.name];
+    if (value === undefined || value === null) {
+      value = "";
+    }
+
+    if (value === "" && field.initialRender) {
+      if (field.defaultValue !== null) {
+        value = field.defaultValue;
+      }
+    }
+    return (
+      <>
+        <TextField
+          id={field.id}
+          label={field.name}
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+          value={value}
+          onChange={e => {
+            this.checkText(field.name, e.target.value);
+          }}
+        />
+      </>
+    );
+  }
+  createForm() {
+    var markup = this.model.editSource.editableFields.map((field, i) => {
+      var valueMarkup = this.getValueMarkup(field);
+      return (
+        <div key={i} ref={field.name}>
+          {valueMarkup}
+        </div>
+      );
+    });
+    return <div>{markup}</div>;
   }
 
   showAdvancedSnackbar = message => {
@@ -87,12 +185,39 @@ class MarkisView extends React.PureComponent {
     });
   };
 
-  btnCreateGeom = () => {
-    console.log("Skapa geometri");
-    // We have access to plugin's model:
-    //if (!this.model.isConnected) this.model.connectToHub();
-    //else this.model.disconnectHub();
+  openCreateDialog = () => {
+    this.setState({
+      inCreation: true
+    });
+    this.model.setEditLayer("markistest:column_test_sweco");
+    this.model.activateAdd();
   };
+
+  abortCreation = () => {
+    this.setState({
+      inCreation: false
+    });
+    this.model.deActivateAdd();
+  };
+
+  saveCreated = () => {
+    this.updateFeature();
+    this.model.save();
+    this.props.enqueueSnackbar("Avtalsgeometrin skapades utan problem!");
+    this.model.deActivateAdd();
+    this.reset();
+  };
+
+  reset() {
+    this.setState({
+      inCreation: false,
+      mode: "visningsläge",
+      contractId: "",
+      contractType: "",
+      enableCreate: false,
+      formValues: {}
+    });
+  }
 
   renderInfoText() {
     if (this.state.mode === "editeringsläge") {
@@ -118,6 +243,73 @@ class MarkisView extends React.PureComponent {
     }
   }
 
+  clearSearchResult = () => {
+    this.model.clearSearchResult();
+    this.setState({
+      contractId: undefined
+    });
+  };
+
+  renderBtns() {
+    const { classes } = this.props;
+    const enableCreateBtn = (
+      <Button
+        variant="contained"
+        className={classes.createButtons}
+        disabled={!this.state.enableCreate}
+        onClick={this.openCreateDialog}
+      >
+        Skapa geometri
+      </Button>
+    );
+
+    const btnAbort = (
+      <Button
+        variant="contained"
+        className={classes.createButtons}
+        onClick={this.abortCreation}
+      >
+        Avbryt
+      </Button>
+    );
+
+    const btnSave = (
+      <Button
+        variant="contained"
+        className={classes.createButtons}
+        onClick={this.saveCreated}
+      >
+        Spara
+      </Button>
+    );
+
+    const btnRemoveResult = (
+      <Button
+        variant="contained"
+        className={classes.createButtons}
+        onClick={this.clearSearchResult}
+      >
+        Rensa resultat
+      </Button>
+    );
+
+    if (this.state.mode === "editeringsläge") {
+      if (!this.state.inCreation) {
+        return <div>{enableCreateBtn}</div>;
+      } else {
+        return (
+          <div>
+            {this.createForm()}
+            {btnAbort}
+            {btnSave}
+          </div>
+        );
+      }
+    } else {
+      return <div>{btnRemoveResult}</div>;
+    }
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -126,13 +318,7 @@ class MarkisView extends React.PureComponent {
           <h3>Du är i {this.state.mode}</h3>
           {this.renderInfoText()}
         </div>
-        <Button
-          variant="contained"
-          disabled={!this.state.enableCreate}
-          onClick={this.btnCreateGeom}
-        >
-          Skapa geometri
-        </Button>
+        {this.renderBtns()}
       </>
     );
   }
