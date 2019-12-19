d665a3bf933037c070cdd805ab419948364ef3a1
Henrik Hallberg <henrik.hallberg@sweco.se>
2019-12-19 08:08:32 +0100

    Markis-tool is now only active when sid is supplied

--- new-client/src/plugins/markis/markis.js
+++ new-client/src/plugins/markis/markis.js
@@ -2,10 +2,8 @@
 import React from "react";
 import BaseWindowPlugin from "../BaseWindowPlugin";
 
-// All plugins need some icon – make sure to pick a relevant one from Material UI Icons
-import BugReportIcon from "@material-ui/icons/BugReport";
+import DesktopWindowsIcon from "@material-ui/icons/DesktopWindows";
 
-// Finally plugin-specific imports. Most plugins will need Model, View and Observer.
 import MarkisModel from "./MarkisModel";
 import MarkisView from "./MarkisView";
 import Observer from "react-event-observer";
@@ -46,33 +44,32 @@ class Markis extends React.PureComponent {
   }
 
   render() {
-    return (
-      <BaseWindowPlugin
-        {...this.props} // Pass on all props
-        type="markis"
-        custom={{
-          icon: <BugReportIcon />, // Custom icon for this plugin
-          title: "Markis", // Custom title, etc
-          description: "Markisanslutning",
-          height: 470, // Custom height/width etc | Use "auto" for automatic or leave undefined
-          width: 400,
-          top: undefined, // If undefined, it will fallback to BaseWindowPlugin's defaults
-          left: undefined
-        }}
-      >
-        {/* This is the child object of BaseWindowPlugin. It will be displayed
-            as content inside the plugin's window. */}
-        <MarkisView
-          // Here we send some props to the plugin's View.
-          // Make sure to ONLY include props that are ACTUALLY USED in the View.
-          model={this.markisModel} // We can supply our model
-          app={this.props.app} // Or even the whole App
-          localObserver={this.localObserver} // And also the Observer, so that those 2 can talk through it
-        />
-      </BaseWindowPlugin>
-    );
+    if (this.sessionId) {
+      return (
+        <BaseWindowPlugin
+          {...this.props}
+          type="markis"
+          custom={{
+            icon: <DesktopWindowsIcon />,
+            title: "Klientkoppling",
+            description: "Markisanslutning",
+            height: 470,
+            width: 400,
+            top: undefined,
+            left: undefined
+          }}
+        >
+          <MarkisView
+            model={this.markisModel}
+            app={this.props.app}
+            localObserver={this.localObserver}
+          />
+        </BaseWindowPlugin>
+      );
+    } else {
+      return <div></div>;
+    }
   }
 }
 
-// Part of API. Make a HOC of our plugin.
 export default Markis;
